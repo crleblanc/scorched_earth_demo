85511203e80c4f0cdacf7cd7eef946a8a248e126
Chris LeBlanc <crleblanc@gmail.com>
2019-10-01 08:02:08 +0100

    Partially working tank image, needs work.

--- scorched_earth.py
+++ scorched_earth.py
@@ -13,17 +13,18 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 GROUND = (125, 125, 125)
+TANK_SIZE = (50, 50)
 
 # Super simple game intended for teaching an 11 year old
 
-def update_bullet(tank, bullet, v_initial, angle, time_val):
+def update_bullet(tank_x, tank_y, bullet, v_initial, angle, time_val):
     """ See https://en.wikipedia.org/wiki/Projectile_motion for the equation 'y = ax + bx^2'"""
 
     x = v_initial * time_val * math.cos(math.radians(angle))
     y = v_initial * time_val * math.sin(math.radians(angle)) - 0.5 * G * time_val**2
 
-    bullet.x = tank.x + x
-    bullet.y = tank.y - y
+    bullet.x = tank_x + x
+    bullet.y = tank_y - y
 
 def explosion(screen, bullet, enemy_tank):
     """ Make an explosion if the bullet hits the enemy tank"""
@@ -65,14 +66,17 @@ def main():
 
     # Origin is in the upper left.  These are the starting positions, we can update them later
     my_tank_coord = terrain[randint(0, len(terrain)-4)]
-    my_tank_left, my_tank_top, my_tank_width, my_tank_height = (my_tank_coord[0], my_tank_coord[1], 50, 50)
-    my_tank = pygame.Rect(my_tank_left, my_tank_top, my_tank_width, my_tank_height)
+    my_tank_left, my_tank_top, my_tank_width, my_tank_height = (my_tank_coord[0], my_tank_coord[1], TANK_SIZE[0], TANK_SIZE[1])
+    #my_tank = pygame.Rect(my_tank_left, my_tank_top, my_tank_width, my_tank_height)
+    my_turret = (my_tank_left + my_tank_width/2, my_tank_left + my_tank_height/2)
+    my_tank = pygame.image.load('tank1.png')
+    screen.blit(my_tank, my_turret)
 
     enemy_tank_coord = terrain[randint(0, len(terrain)-4)]
-    enemy_tank_left, enemy_tank_top, enemy_tank_width, enemy_tank_height = (enemy_tank_coord[0], enemy_tank_coord[1], 50, 50) 
+    enemy_tank_left, enemy_tank_top, enemy_tank_width, enemy_tank_height = (enemy_tank_coord[0], enemy_tank_coord[1], TANK_SIZE[0], TANK_SIZE[1]) 
     enemy_tank = pygame.Rect(enemy_tank_left, enemy_tank_top, enemy_tank_width, enemy_tank_height)
 
-    pygame.draw.rect(screen, BLACK, my_tank)
+#    pygame.draw.rect(screen, BLACK, my_tank)
     pygame.draw.rect(screen, BLACK, enemy_tank)
     print(enemy_tank_coord)
 
@@ -113,7 +117,7 @@ def main():
             bullet = pygame.Rect(my_tank_left, my_tank_top, 5, 5)
             while bullet.y < WINDOWHEIGHT:
                 time_diff = (time.time() - start_time) * 10
-                update_bullet(my_tank, bullet, v_initial, angle, time_diff)
+                update_bullet(my_tank_left, my_tank_top, bullet, v_initial, angle, time_diff)
 
                 pygame.draw.rect(screen, RED, bullet)
 
@@ -128,7 +132,8 @@ def main():
         text_surface = font.render('Angle: %d, Velocity: %d' % (angle, v_initial), True, BLACK, 0)
         screen.blit(text_surface, text_rect)
         pygame.draw.polygon(screen, GROUND, terrain)
-        pygame.draw.rect(screen, BLACK, my_tank)
+#        pygame.draw.rect(screen, BLACK, my_tank)
+        screen.blit(my_tank, my_turret)
         pygame.draw.rect(screen, BLACK, enemy_tank)
         pygame.time.wait(50)
         pygame.display.update()
